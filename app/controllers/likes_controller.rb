fefaf2776539e0a28edb4304551d2666e71c5924
Christine Kim <ckim31093@gmail.com>
2019-03-14 11:05:55 -0400

    Create like create and destroy methods

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,25 +1,40 @@
 class LikesController < ApplicationController
+  def new
+    @like = Like.new
+
+    render 'dogs/likes/new'
+  end
+
   def create
-    @like = current_user.likes.new(like_params)
+    @like = current_user.likes.new(dog_id: params[:dog_id])
+    @dog = Dog.find(params[:dog_id])
+     
 
-    respond_to do |format|
-      if @like.save
-
+    if current_user.likes.where(dog_id: params[:dog_id]).length == 0
+      @like.save
+      respond_to do |format|
         format.html { redirect_to @dog, notice: 'Like was successfully created.' }
         format.json { render :show, status: :created, location: @dog }
-      else
-        format.html { render :new }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to @dog, notice: 'Already liked this dog, cannot like more than once' }
         format.json { render json: @like.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def destroy
-  end
+    @like = current_user.likes.where(dog_id: params[:dog_id])[0]
+    @dog = Dog.find(params[:dog_id])
 
-  private
-  def like_params
-    params.require(:like).permit(:user_id, :dog_id)
+    if @like
+      @like.destroy
+      respond_to do |format|
+        format.html { redirect_to @dog, notice: 'Like was successfully unliked.' }
+        format.json { render :show, status: :created, location: @dog }
+      end
+    end
   end
 
 end
